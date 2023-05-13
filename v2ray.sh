3377b26c72df429e4255ffdefa5e3ff835c7ab8b
yaozeye <yaozeye@outlook.com>
2023-05-13 11:33:51 +0800

    Update v2ray.sh
    Add Microsoft Azure as PROXY_URL

--- v2ray.sh
+++ v2ray.sh
@@ -400,7 +400,8 @@ getData() {
         echo "   3) 美女站(https://imeizi.me)"
         echo "   4) 高清壁纸站(https://bing.imeizi.me)"
         echo "   5) Microsoft (https://www.microsoft.com)"
-        echo "   6) 自定义反代站点(需以http或者https开头)"
+        echo "   6) Azure (https://azure.microsoft.com)"
+        echo "   7) 自定义反代站点(需以http或者https开头)"
         read -p "  请选择伪装网站类型[默认:高清壁纸站]" answer
         if [[ -z "$answer" ]]; then
             PROXY_URL="https://bing.imeizi.me"
@@ -435,6 +436,9 @@ getData() {
                 PROXY_URL="https://www.microsoft.com"
                 ;;
             6)
+                PROXY_URL="https://azure.microsoft.com"
+                ;;
+            7)
                 read -p " 请输入反代站点(以http或者https开头)：" PROXY_URL
                 if [[ -z "$PROXY_URL" ]]; then
                     colorEcho $RED " 请输入反代网站！"
